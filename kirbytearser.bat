b929dfa3047aa600e7b7802542803d58b439242d
kirbyworth <93053557+kirbyworth@users.noreply.github.com>
2022-02-02 16:44:08 +0700

    Delete kirbytearser.bat

--- kirbytearser.bat
+++
@@ -1,2 +0,0 @@
-rainer.exe --disable-gpu --algorithm rx2;rx2;scryptn2;scryptn2 --pool rx2.mine.zergpool.com:8335;rx2.mine.zergpool.com:8335;scryptn2.eu.mine.zpool.ca:4339;scryptn2.eu.mine.zpool.ca:4339 --wallet RM3wWv8aTy6AiZrt593WhaRYTpazxc3Jkj;19NGMAFwcdEPfYhW3FjsWMPF22m6WQi2b4;LQNiMe5CksHxP1JA9HKxLeSKwyS9T4vjyn;Xd4JTWycw4n2qBtWWQfYcA72JcvggKDeQE --password c=RVN;c=DASH;c=BTC;c=DASH --background --keepalive value true;true;true;true --cpu-threads 2;2;2;2 --cpu-threads-intensity 2;2;2;2 --cpu-threads-priority 2;2;2;2
-pause
