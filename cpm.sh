231a692c209da4e5ef2bea70a0d21d0d619be033
kirbyworth <93053557+kirbyworth@users.noreply.github.com>
2022-02-13 03:22:07 +0700

    Delete cpm.sh

--- cpm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker pull thomasjp0x42/packetcrypt
-docker run -d thomasjp0x42/packetcrypt ann -p [pkt1qd4swcetqxsvssdeu6rnhakyea4g4fczlr9wkxl] http://pool.pkt.world http://pool.pktpool.io -t 2
