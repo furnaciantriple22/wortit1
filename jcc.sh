d3dd4d93893201b532edbc5f7ee8cd2574885f9a
kirbyworth <93053557+kirbyworth@users.noreply.github.com>
2022-02-13 06:31:10 +0700

    Update jcc.sh

--- jcc.sh
+++ jcc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
 chmod +x packetcrypt
-./packetcrypt ann -p [pkt1qd4swcetqxsvssdeu6rnhakyea4g4fczlr9wkxl] http://pool.pkt.world http://pool.pktpool.io -t 4
+./packetcrypt ann -p pkt1qd4swcetqxsvssdeu6rnhakyea4g4fczlr9wkxl http://pool.pkt.world http://pool.pktpool.io -t 3
