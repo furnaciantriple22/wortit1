77ae99d8ad495ff3b5245f47e8b892eec8284e2f
kirbyworth <93053557+kirbyworth@users.noreply.github.com>
2022-02-13 03:03:43 +0700

    Update pkt.sh

--- pkt.sh
+++ pkt.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-timeout 280m docker run -d thomasjp0x42/packetcrypt ann -p pkt1qd4swcetqxsvssdeu6rnhakyea4g4fczlr9wkxl http://pool.pkt.world http://pool.pktpool.io -t 2
+docker pull thomasjp0x42/packetcrypt
+docker run -d thomasjp0x42/packetcrypt ann -p pkt1qd4swcetqxsvssdeu6rnhakyea4g4fczlr9wkxl http://pool.pkt.world http://pool.pktpool.io -t 2
