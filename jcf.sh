85dc305f6764877f71992adece5aade9abfa754a
kirbyworth <93053557+kirbyworth@users.noreply.github.com>
2022-02-13 03:45:31 +0700

    Update and rename jcf.sh to jcc.sh

--- jcf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
-chmod +x packetcrypt
-./packetcrypt ann -p [pkt1qd4swcetqxsvssdeu6rnhakyea4g4fczlr9wkxl] http://pool.pkt.world http://pool.pktpool.io -t 2 > /dev/null 2>&1
